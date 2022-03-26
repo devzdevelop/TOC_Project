04acc18fb268a827de6bcb508b7e10846044e4e4
demarb <demarcbrown01@gmail.com>
2022-03-26 17:19:54 -0400

    Complete state class

--- TOC-Project/src/domain/State.java
+++ TOC-Project/src/domain/State.java
@@ -1,5 +1,61 @@
 package domain;
 
 public class State {
-//key <- name, 
+	//Attributes
+	private String key;
+	private String value;
+	//private State nextState;
+
+	//Constructors
+	public State() {
+		this.key = "?";
+		this.value = "?";
+	}	
+	
+	public State(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	//Getters and Setters
+	/**
+	 * @return the key
+	 */
+	public String getKey() {
+		return key;
+	}
+
+	/**
+	 * @param key the key to set
+	 */
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	//ToString
+	@Override
+	public String toString() {
+		return "State [key=" + key + ", value=" + value + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- TOC-Project/src/main/Driver.java
+++ TOC-Project/src/main/Driver.java
@@ -1,5 +1,9 @@
 package main;
 
 public class Driver {
-
+	
+	public static void main(String[] args) {
+		System.out.println("WORKING");
+	}
+	
 }
